29b5f5f982a7122f8b667daf6cea2b77d2aea5e4
Tu Nombre <tuemail@example.com>
2026-09-08 22:28:06 -0300

    mm

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,10 +1,14 @@
-javascript
 const courses = [
     {
         subject: "CSE",
         number: 110,
         title: "Introduction to Programming",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.",
+        technology: [
+            "Python"
+        ],
         completed: true
     },
     {
@@ -12,6 +16,12 @@ const courses = [
         number: 130,
         title: "Web Fundamentals",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming.",
+        technology: [
+            "HTML",
+            "CSS"
+        ],
         completed: true
     },
     {
@@ -19,6 +29,11 @@ const courses = [
         number: 111,
         title: "Programming with Functions",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call, debug, and test their own functions.",
+        technology: [
+            "Python"
+        ],
         completed: true
     },
     {
@@ -26,6 +41,11 @@ const courses = [
         number: 210,
         title: "Programming with Classes",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.",
+        technology: [
+            "C#"
+        ],
         completed: false
     },
     {
@@ -33,13 +53,27 @@ const courses = [
         number: 131,
         title: "Dynamic Web Fundamentals",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.",
+        technology: [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
         completed: true
     },
     {
         subject: "WDD",
         number: 231,
-        title: "Web Frontend Development I",
+        title: "Frontend Web Development I",
         credits: 2,
+        certificate: "Web and Computer Programming",
+        description: "This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.",
+        technology: [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
         completed: false
     }
 ];
